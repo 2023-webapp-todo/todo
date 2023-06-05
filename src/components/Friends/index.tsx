90ff28d7b34104660c0364ed98a969786e7d0d10
sangminlee98 <tkdals0975@naver.com>
2023-06-06 00:09:35 +0900

    feat : Todo페이지 friend 리스트 UI

---
+++ src/components/Friends/index.tsx
@@ -0,0 +1,13 @@
+import styles from "./styles.module.scss";
+
+export default function Friends() {
+  return (
+    <ul className={styles.container}>
+      <li>무지</li>
+      <li>레오</li>
+      <li>라이언</li>
+      <li>프로도</li>
+      <li>춘식</li>
+    </ul>
+  );
+}
--- src/pages/Todo/index.tsx
+++ src/pages/Todo/index.tsx
@@ -2,21 +2,20 @@ import { Navigate, useOutletContext } from "react-router-dom";
 import Calender from "@/components/Calendar";
 import styles from "./styles.module.scss";
 import Feed from "@/components/Feed";
+import { useRecoilValue } from "recoil";
+import selectedProfileState from "@/stores/selectedProfile";
+import Friends from "@/components/Friends";
 
 export default function Todo() {
   const authState = useOutletContext();
+  const selectedProfile = useRecoilValue(selectedProfileState);
   // if (!authState) {
   //   return <Navigate to="/login" />;
   // }
   return (
     <div className={styles.container}>
-      <ul className={styles.friends}>
-        <li>무지</li>
-        <li>레오</li>
-        <li>라이언</li>
-        <li>프로도</li>
-        <li>춘식</li>
-      </ul>
+      <Friends />
+      <h1 className={styles.username}>{selectedProfile}의 TODO</h1>
       <div className={styles.content}>
         <Calender />
         <Feed />
